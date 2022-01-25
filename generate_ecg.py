c26159a37ee803de9da3378573b3cfe523a6229d
seang097 <sean.gardner098@gmail.com>
2022-01-25 12:17:38 -0300

    move to one module, working

--- generate_ecg.py
+++ generate_ecg.py
@@ -1,5 +1,6 @@
 
 from signalz import ecgsyn
+import paper_ecg
 import numpy as np
 import time
 import math
@@ -12,17 +13,20 @@ sfint = 100
 n = math.ceil(hrmean*duration/60)
 rhythm_lead = 1
 
-def generate_ecg(values, text):
+def generate_ecg_values(values):
     signals = []
     rhythm_signal = []
     for lead in range(0,12):
-        y = ecgsyn(**values[lead])[0][:math.ceil(sfecg*duration)]
+        y = ecgsyn(**values[lead])[0][:math.ceil(sfecg*duration)]# is there room for performance imporvements here? We seem to generate more ekg than we use
         signals.append(y)
 
     signals_arr = np.array(signals)
     
-    print(signals_arr.shape)
-    print(rhythm_signal)
+    return signals_arr
+
+def generate_ecg_plot(signals_arr,title):
+    plot(signals_arr, sample_rate=sfecg, title=title)
+    save_as_svg("test_svg")
 
 test_values_lead = {"sfecg" : sfecg,
     "n" : n,
@@ -38,7 +42,9 @@ test_values_lead = {"sfecg" : sfecg,
 test_values = {lead: test_values_lead for lead in range(0,12)}
 t0 = time.time()
 
-generate_ecg(test_values, "Georgey")
+signal = generate_ecg_values(test_values)
+paper_ecg.plot(signal,sample_rate=sfecg,title="Georgey")
+paper_ecg.save_as_svg("test_svg")
 t1 = time.time()
 
 print(t1-t0)
